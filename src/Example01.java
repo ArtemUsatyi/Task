ddb684040e7c69eb0025e5e50e130ddb3a714346
Artemo <artemo180791@mail.ru>
2022-07-25 14:37:02 +0300

    Найти наименьшее число в массиве

--- src/Example01.java
+++ src/Example01.java
@@ -1,11 +1,10 @@
 public class Example01 {
-    int[] mas = new int[]{10, 20, 9, 1, 7, 11, 5, 3, 2};
+    int[] mas = new int[]{10, 20, 9, 2, 7, 11, 5, 0, 2};
     int rezult = mas[0];
 
     public void minNumber() {
         for (int i = 0; i < mas.length; i++) {
-            if (rezult < mas[i]) {
-            } else rezult = mas[i];
+            if (rezult > mas[i]) rezult = mas[i];
         }
         System.out.println(rezult);
     }
