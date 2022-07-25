3f15a1fcfb0f60317d46cbb170629c13193bf25d
Artemo <artemo180791@mail.ru>
2022-07-25 15:36:58 +0300

    Является ли число Палиндромом

--- src/Example02.java
+++ src/Example02.java
@@ -1,3 +1,21 @@
 public class Example02 {
+    private int sum = 0;
+    private int inverter;
 
+    public void setSum(int sum) {
+        if (sum > 0) this.sum = sum;
+        else {
+            System.out.println("Число : " + sum + " - не является палиндромом");
+        }
+    }
+
+    public void palindrome(int palindrome) {
+        while (sum > 0) {
+            int remainder = sum % 10;
+            inverter = (inverter * 10) + remainder;
+            sum = sum / 10;
+        }
+        if (inverter == palindrome) System.out.println("Число: " + palindrome + " - является палиндромом");
+        else System.out.println("Число: " + palindrome + " НЕ - является палиндромом");
+    }
 }
