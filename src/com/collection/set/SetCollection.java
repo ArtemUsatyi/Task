6d0a4bb36a8f6d6763433aa4afc7d91e14551da4
Artemo <artemo180791@mail.ru>
2022-08-22 18:31:42 +0300

    Set Collection - объединение, разность, пересечение

--- src/com/collection/set/SetCollection.java
+++ src/com/collection/set/SetCollection.java
@@ -11,8 +11,15 @@ public class SetCollection {
         Set<Integer> linkedHashSetId = new LinkedHashSet<>();
         Set<Integer> treeHashSetId = new TreeSet<>();
 
+        linkedHashSetId.add(100);
+        linkedHashSetId.add(1);
+        linkedHashSetId.add(2);
+        linkedHashSetId.add(3);
+        linkedHashSetId.add(4);
+        linkedHashSetId.add(5);
+        linkedHashSetId.add(6);
+
         setMethod(hashSetId);
-        setMethod(linkedHashSetId);
         setMethod(treeHashSetId);
         System.out.println("Метод add() у HashSep - вызывает метод put() у HashMap, НО без ключа!");
         System.out.println("HashSet");
@@ -22,22 +29,40 @@ public class SetCollection {
         System.out.println("TreeSet");
         System.out.println("При добавлении сортирует список: " + treeHashSetId);
         System.out.println("Другие методы");
-        System.out.println("Возвращает True/False, если найдено значение: " + hashSetId.contains(22));
+        System.out.println("Возвращает True/False, если найдено значение: " + hashSetId.contains(5));
         System.out.println("Размер списка: " + linkedHashSetId.size());
         System.out.println("Получить HashCode HashSet: " + hashSetId.hashCode());
         System.out.println("Получить HashCode LinkedHashSet: " + linkedHashSetId.hashCode());
-        System.out.println("Удаляет элемент по значению и возвращает True/False: " + treeHashSetId.remove(1));
+        System.out.println("Удаляет элемент по значению и возвращает True/False: " + treeHashSetId.remove(10));
         System.out.println("Сохраняет порядок при добавлении, но не сортирует: " + treeHashSetId);
         System.out.println("Выводит первый элемент в списке: " + linkedHashSetId.iterator().next());
+
+        // объединение множеств
+        Set<Integer> union = new LinkedHashSet<>(linkedHashSetId);
+        System.out.println(union);
+        System.out.println("Выводит True/False если произошло добавление: "  + union.addAll(treeHashSetId));
+        System.out.println(union);
+
+        // intersection -пересечение множеств
+        Set<Integer> intersection = new LinkedHashSet<>(linkedHashSetId);
+        System.out.println("Выводит True/False если произошло пересечение: "  + intersection.retainAll(treeHashSetId));
+        System.out.println(intersection);
+
+        // difference - пересечение множеств
+        Set<Integer> difference = new LinkedHashSet<>(linkedHashSetId);
+        System.out.println("Выводит True/False если произошло разность (вычитание): "  + difference.removeAll(treeHashSetId));
+        System.out.println(difference);
+
+
     }
 
     private static void setMethod(Set<Integer> list) {
-        list.add(133);
-        list.add(22);
-        list.add(1);
-        list.add(42);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        list.add(8);
         list.add(9);
-        list.add(25);
-        list.add(25); // не добавляет одинаковые значения в список
+        list.add(10); // не добавляет одинаковые значения в список
     }
 }
